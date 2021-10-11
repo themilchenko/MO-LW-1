98c5be41c34f8f0a1c952ce98ed6c75aba7823c2
Ivan <70635146+themilchenko@users.noreply.github.com>
2021-10-11 12:38:55 +0300

    Update Symplex.h

--- MO_LAB1/Symplex.h
+++ MO_LAB1/Symplex.h
@@ -9,43 +9,46 @@
 
 class Symplex
 {
-	/*матрица преобразований*/
-	std::vector<std::vector<double>> table_;
+    /*матрица преобразований*/
+    std::vector<std::vector<double>> table_;
+    std::vector<double> goal_func;
 
-	/*резрешающие таблица и строка*/
-	int permissive_str_;
-	int permissive_column_;
+    /*резрешающие таблица и строка*/
+    int permissive_str_;
+    int permissive_column_;
 
-	/*вектора переменных для таблицы*/
-	std::vector<std::string> basis_;
-	std::vector<std::string> free_;
+    /*вектора переменных для таблицы*/
+    std::vector<std::string> basis_;
+    std::vector<std::string> free_;
 
 public:
-	/*конструктор для сборки таблицы из файла*/
-	Symplex(std::ifstream& input);
+    /*конструктор для сборки таблицы из файла*/
+    Symplex(std::ifstream& input);
 
-	/*проверка на оптимальность*/
-	bool is_optimal() const;
+    /*проверка на оптимальность*/
+    bool is_optimal() const;
 
-	/*печать таблицы*/
-	void print() const;
+    /*печать таблицы*/
+    void print() const;
 
-	/*поиск разрешающей колнки и сроки*/
-	int find_column() const;
-	int find_str() const;
+    /*поиск разрешающей колнки и сроки*/
+    int find_column() const;
+    int find_str() const;
 
-	/*одна итерация преобразования*/
-	void do_step();
+    /*одна итерация преобразования*/
+    void do_step();
 
-	/*геттер оптимального решения*/
-	double get_solution() const;
+    /*геттер оптимального решения*/
+    double get_solution() const;
 
-	~Symplex()
-	{
-		for (auto& i : table_)
-			i.clear();
-		table_.clear();
-		basis_.clear();
-		free_.clear();
-	}
+    void do_examination() const;
+
+    ~Symplex()
+    {
+        for (auto& i : table_)
+            i.clear();
+        table_.clear();
+        basis_.clear();
+        free_.clear();
+    }
 };
